2da6626b1e10dc034668713eb25987f67ef68c32
silvanshade <silvanshade@users.noreply.github.com>
2024-04-14 17:34:59 -0600

    Add treefmt-nix for formatting and checks

--- flake.nix
+++ flake.nix
@@ -2,17 +2,30 @@
   description = "This flake provides bootstrap files with applies fixes for xthead extensions.";
 
   inputs = {
-    gcc13-git.flake = false;
-    gcc13-git.url = "git://gcc.gnu.org/git/gcc.git?ref=releases/gcc-13";
     nixpkgs.url = "github:nixos/nixpkgs/master";
+    gcc13-git = {
+      url = "git://gcc.gnu.org/git/gcc.git?ref=releases/gcc-13";
+      flake = false;
+    };
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { nixpkgs, systems, gcc13-git, ... }:
+  outputs = { self, nixpkgs, systems, gcc13-git, treefmt-nix, ... }:
     let
       crossSystem = "riscv64-linux";
       eachSystem = nixpkgs.lib.genAttrs (import systems);
+      treefmtEval = eachSystem (system: treefmt-nix.lib.evalModule nixpkgs.legacyPackages.${system} ./treefmt.nix);
     in
     {
+      formatter = eachSystem (system: treefmtEval.${system}.config.build.wrapper);
+
+      checks = eachSystem (system: {
+        formatting = treefmtEval.${system}.config.build.check self;
+      });
+
       packages = eachSystem (system:
         let
           pkgs = import nixpkgs {
---
+++ treefmt.nix
@@ -0,0 +1,7 @@
+{
+  projectRootFile = "flake.nix";
+  programs.deadnix.enable = true;
+  programs.mdformat.enable = true;
+  programs.nixpkgs-fmt.enable = true;
+  programs.statix.enable = true;
+}
